1c4a52b3d016c4868405179fec40fe0d538e6d34
starpolar <starpolar24@gmail.com>
2024-07-20 23:51:50 +0900

    add

--- Study/Algorithms/C++_Code/24_07/11~/20.cpp
+++ Study/Algorithms/C++_Code/24_07/11~/20.cpp
@@ -1,19 +1,43 @@
 // 25304 영수증
 // 반복문
+/* 메모리 소비가 더 큼 안쓰는걸로.
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x, n, total = 0;
+    cin >> x;
+    cin >> n;
+
+    for (int _ = 0; _ < n; _++) {
+        int a, b;
+        cin >> a >> b;
+        total += a * b;
+    }
+    if (x == total) {
+        cout << "Yes" << endl;
+    } else {
+        cout << "No" << endl;
+    }
+
+    return 0;
+}
+ */
+
 #include <cstdio>
 
 int main() {
-    long long total, cnt = 0;
+    int total, cnt = 0;
     int n;
     scanf("%d", &total);
     scanf("%d", &n);
-    printf("total : %d\n", total);
+    // printf("total : %d\n", total);
 
     for (int _ = 0; _ < n; _++) {
-        long long a = 0, b = 0;
+        int a, b;
         scanf("%d %d", &a, &b);
         cnt += a * b;
-        printf("%d and %d\n", cnt, _);
+        // printf("%d and %d\n", cnt, _);
     }
     if (total == cnt) {
         printf("Yes");
