2134d435ef594d3dcd029283c4caf00620f624c0
starpolar <starpolar24@gmail.com>
2024-07-24 13:55:22 +0900

    add

---
+++ Study/Algorithms/C++_Code/24_07/21~/24.cpp
@@ -0,0 +1,21 @@
+// 11022	A+B - 8
+// ¹Ýº¹¹®
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+
+    for (int i = 1; i <= t; i++) {
+        int a, b;
+        cin >> a >> b;
+
+        cout << "Case #" << i << 
+        ": " << a << " + " << b <<
+        " = " << a + b << "\n";
+    }
+
+    return 0;
+}
