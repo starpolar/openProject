66e91f88e0b066cbe1539e4d09671ac9fd99b2fa
starpolar <starpolar24@gmail.com>
2024-08-06 20:23:25 +0900

    stu

---
+++ Study/Algorithms/TIL_Coding/TIL_Coding/7월/28.cpp
@@ -0,0 +1,19 @@
+﻿//10951	A + B - 4
+//반복문
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a, b;
+	for (; ;) {
+		if (cin >> a >> b) {
+			cout << a + b << '\n';
+		}
+		else {
+			break;
+		}
+	}
+	
+	return 0;
+}
